bee81a8be6e8ff0e433ec646601b9a8459df8e40
corselia <takiya@toran.sakura.ne.jp>
2020-05-03 21:59:32 +0900

    Fix reviewed items

--- src/components/parts/AboutIllustrationsCountingOfVotes.vue
+++ src/components/parts/AboutIllustrationsCountingOfVotes.vue
@@ -6,7 +6,14 @@
           開票イラストについて
         </p>
         <div class="text--primary">
-          今年も開票イラストを募集します。詳細は準備中です。アナウンスをお待ち下さい。
+          今年も開票イラストを募集します。詳細は後ほど記載します。
+          <div class="text--primary">
+            最新の情報は<a
+              href="https://twitter.com/gensosenkyo"
+              target="_blank"
+              >主催アカウント (@gensosenkyo) によるツイート</a
+            >をご覧ください。
+          </div>
         </div>
       </v-col>
     </v-row>
--- src/components/parts/SiteAttentions.vue
+++ src/components/parts/SiteAttentions.vue
@@ -6,11 +6,14 @@
           本ページの情報について
         </p>
         <div class="text--primary">
-          本ページに記載されている情報は最新でない可能性があります。最新の情報は<a
-            href="https://twitter.com/search?q=%23%E5%B9%BB%E6%B0%B4%E7%B7%8F%E9%81%B8%E6%8C%992020_%E4%B8%BB%E5%82%AC%E3%82%88%E3%82%8A&src=typed_query&f=live"
-            target="_blank"
-            >主催アカウントによるツイート</a
-          >をご覧ください。
+          本ページに記載されている情報は最新でない可能性があります。
+          <div class="text--primary">
+            最新の情報は<a
+              href="https://twitter.com/gensosenkyo"
+              target="_blank"
+              >主催アカウント (@gensosenkyo) によるツイート</a
+            >をご覧ください。
+          </div>
         </div>
       </v-col>
     </v-row>
