7b142640b69a3b22006bace9864c6ffa97a70af8
TAKIYA Osamu <takiya@toran.sakura.ne.jp>
2020-05-03 21:11:49 +0900

    Add past event websites to navbar (#22)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -36,6 +36,24 @@
               <v-list-item-title>幻水総選挙 2019（別サイト）</v-list-item-title>
             </v-list-item>
           </a>
+
+          <a href="https://election-2018.suikoden.info/" target="_blank">
+            <v-list-item>
+              <v-list-item-title>幻水総選挙 2018（別サイト）</v-list-item-title>
+            </v-list-item>
+          </a>
+
+          <a href="https://election-2017.suikoden.info/" target="_blank">
+            <v-list-item>
+              <v-list-item-title>幻水総選挙 2017（別サイト）</v-list-item-title>
+            </v-list-item>
+          </a>
+
+          <a href="https://corselia.sakura.ne.jp/gensui/sosenkyo_2016_result/index.php" target="_blank">
+            <v-list-item>
+              <v-list-item-title>幻水総選挙 2016（別サイト）</v-list-item-title>
+            </v-list-item>
+          </a>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
